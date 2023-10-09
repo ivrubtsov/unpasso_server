21137b9d8d4258445c5cf6695b687e7bf0125c33
ivrubtsov <ilya.v.rubtsov@gmail.com>
2023-10-09 20:21:24 -0300

    l19

--- user.py
+++ user.py
@@ -605,7 +605,7 @@ def check_auth(username, password):
     if cursor.rowcount>0:
         res = cursor.fetchall()
         for (db_id, db_username, db_password) in res:
-            if db_id and db_username and db_password == wp_crypt.crypt_private(password):
+            if db_id and db_username and db_password == wp_crypt.crypt_private(password, db_password):
                 auth = True
     return auth
 
