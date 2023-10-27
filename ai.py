8c3e42ca511260704557b939a30d37a111a36752
ivrubtsov <ilya.v.rubtsov@gmail.com>
2023-10-26 21:59:51 -0300

    int temperature

--- ai.py
+++ ai.py
@@ -13,7 +13,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=os.getenv('OPENAI_TEMPERATURE'), # this is the degree of randomness of the model's output
+        temperature=int(os.getenv('OPENAI_TEMPERATURE')), # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
 
